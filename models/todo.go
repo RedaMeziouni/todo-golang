a521df49701f8b6c2a9e7c8be290a2415e2dc0c6
RedaMeziouni <meziounidb@gmail.com>
2022-02-13 15:52:29 +0100

    Get Todos by ID

--- models/todo.go
+++ models/todo.go
@@ -19,6 +19,17 @@ func GetTodos(c *fiber.Ctx) error {
 
 }
 
+func GetTodoById(c *fiber.Ctx) error {
+	id := c.Params("id")
+	db := database.DBConn
+	var todo Todo
+	err := db.Find(&todo, id).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Cound Not find Todo!", "data": err})
+	}
+	return c.JSON(&todo)
+}
+
 func CreateTodo(c* fiber.Ctx) error {
 	db := database.DBConn
 	todo := new(Todo)
